084c3e437408a383097616ef82baaa109cead368
netomurilo13 <netomurilo100@gmail.com>
2023-05-05 03:56:35 -0300

    change timer

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 @Module({
   imports: [UserModule, JwtModule.register({
     secret: process.env.JWT_SECRET,
-    signOptions: {expiresIn: '1h'}
+    signOptions: {expiresIn: '2h'}
   })],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
