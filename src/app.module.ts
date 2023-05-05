60a7afecf87f74238bdcd4ee0a6bab6da0d6444a
netomurilo13 <netomurilo100@gmail.com>
2023-05-05 12:53:09 -0300

    feat(expense) create files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { ExpenseModule } from './expense/expense.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, ExpenseModule],
   controllers: [AppController],
   providers: [
     AppService,
